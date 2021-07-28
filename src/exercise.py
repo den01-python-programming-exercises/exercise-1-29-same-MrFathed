a369b180cd72a5b35ba89f1338125f9368ae437c
Justin Turner <jmturner638@gmail.com>
2021-07-27 22:04:05 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    string1 = input("Enter the first string:")
+    string2 = input("Enter the second string:")
 
+    if string1 == string2:
+        print("Same")
+    else:
+        print("Different")
+        
 if __name__ == '__main__':
     main()
